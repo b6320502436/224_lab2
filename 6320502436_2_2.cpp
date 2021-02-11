83af52b852b51af34efeb7ad3a3423aee76df118
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-11 16:08:44 +0800

    Create 6320502436_2_2.cpp

---
+++ 6320502436_2_2.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+main()
+{
+	char A;
+	float a,b,c,d,e,f;
+	scanf("%c %f",&A,&b);
+	if(A=='A')
+	{
+		b=b-200;
+		c=199+b*3;
+		printf("%.2f",c);
+	}
+	else if(A=='B')
+	{
+		b=b-400;
+		c=299+b*2;
+		printf("%.2f",c);
+	}
+	
+	
+	
+	
+}
