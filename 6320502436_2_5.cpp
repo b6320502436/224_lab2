2fe8fd2ebc72c83ee8cde7d7e43776f65aea4a3a
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-11 18:18:44 +0800

    Update 6320502436_2_5.cpp

--- 6320502436_2_5.cpp
+++ 6320502436_2_5.cpp
@@ -8,20 +8,25 @@ int main()
     	a=a%7;
 	}
 	switch(b){
-        case 1: c=3; break;
-        case 2: c=0; break;
-        case 3: c=3; break;
-        case 4: c=2; break;
-        case 5: c=3; break;
-        case 6: c=2; break;
-        case 7: c=3; break;
-        case 8: c=3; break;
-        case 9: c=2; break;
-        case 10: c=3; break;
-        case 11: c=2; break;
-        case 12: c=3; break;
+        case 1: c=31; break;
+        case 2: c=28; break;
+        case 3: c=31; break;
+        case 4: c=30; break;
+        case 5: c=31; break;
+        case 6: c=30; break;
+        case 7: c=31; break;
+        case 8: c=31; break;
+        case 9: c=30; break;
+        case 10: c=31; break;
+        case 11: c=30; break;
+        case 12: c=31; break;
     }
-    printf("%d",a+c);
+    if((c-a)%7==0)
+    {
+	printf("%d",7);
+    }
+    else
+	printf("%d",(c-a)%7);
 }
 
 	
