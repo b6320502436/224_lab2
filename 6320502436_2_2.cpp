8652eb3a61e16550ba39cb9bbe442d35681861b0
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-11 18:50:06 +0800

    Update 6320502436_2_2.cpp

--- 6320502436_2_2.cpp
+++ 6320502436_2_2.cpp
@@ -8,8 +8,10 @@ main()
 	scanf("%f",&b);
 	if(A=='A')
 	{
-		if(b>200){b=b-200;
-		b=(int)c*3+((int)(c*100));
+		if(b>200){
+		b=b-200;
+		d=(((int)(b*100)%100)/60)*3;
+		b=3*(int)b+d;
 		c=199+b;
 		printf("%.2f",c);}
 		else printf("%.2f",c=199.00);
@@ -18,6 +20,8 @@ main()
 	{
 		if(b>400)
 		{b=b-400;
+		d=(((int)(b*100)%100)/60)*2;
+		b=2*(int)b+d;
 		c=299+b*2;
 		printf("%f",c);
 	}
