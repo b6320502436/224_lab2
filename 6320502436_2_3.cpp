a6fcf7a9835839fb8a85e2d3ae5169f56cd8175d
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-11 17:39:08 +0800

    Update 6320502436_2_3.cpp

--- 6320502436_2_3.cpp
+++ 6320502436_2_3.cpp
@@ -2,15 +2,63 @@
 #include<math.h>
 main()
 {
-	int a,b,c,i;
-	char A,B,C;
-	scanf("%d %d %d",&a,&b,&c);
+	int a,b,c,i,d[3],e,f,max,min;
+	char A,B,C,D[3];
+	scanf("%d %d %d",&d[0],&d[1],&d[2]);
 	fflush(stdin);
-	scanf("%c%c%c",&A,&B,&C);
+	scanf("%s",D);
+	if(d[0]>d[1]&&d[0]>d[2]&&d[1]>d[2])
+	{
+		a=d[2];
+		b=d[1];
+		c=d[0];
+	}
+	if(d[0]>d[1]&&d[0]>d[2]&&d[2]>d[1])
+	{
+		a=d[1];
+		b=d[2];
+		c=d[0];
+	}
+	if(d[1]>d[2]&&d[1]>d[0]&&d[2]>d[0])
+	{
+		a=d[0];
+		b=d[2];
+		c=d[1];
+	}
+	if(d[1]>d[2]&&d[1]>d[0]&&d[0]>d[2])
+	{
+		a=d[2];
+		b=d[0];
+		c=d[1];
+	}
+	if(d[2]>d[1]&&d[2]>d[0]&&d[0]>d[1])
+	{
+		a=d[1];
+		b=d[0];
+		c=d[2];
+	}
+	if(d[2]>d[1]&&d[2]>d[0]&&d[1]>d[0])
+	{
+		a=d[0];
+		b=d[1];
+		c=d[2];
+	}
+	
+	A=D[0];
+	B=D[1];
+	C=D[2];
 	if(A=='A'&&B=='B'&&C=='C')
 	printf("%d %d %d",a,b,c);
 	else if(A=='A'&&B=='C'&&C=='B')
 	printf("%d %d %d",a,c,b);
+	else if(A=='B'&&B=='A'&&C=='C')
+	printf("%d %d %d",b,a,c);
+	else if(A=='B'&&B=='C'&&C=='A')
+	printf("%d %d %d",b,c,a);
+	else if(A=='C'&&B=='A'&&C=='B')
+	printf("%d %d %d",c,a,b);
+	else if(A=='C'&&B=='B'&&C=='A')
+	printf("%d %d %d",c,b,a);
 
 	
 	
