5954f2f10eddf98d06ebd1644c166893c6028149
b6320502436 <78515310+b6320502436@users.noreply.github.com>
2021-02-18 16:39:04 +0800

    Create Untitled4.cpp

---
+++ Untitled4.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c,i,count;
+    char a1[5];
+    int b1[5];
+    scanf("%s",&b);
+    if(a>b)
+    {
+        count=a;
+        a=b;
+        b=count;
+    }
+    if(a>c)
+    {
+        count=a;
+        a=c;
+        c=count;
+    }
+    if(b>c)
+    {
+        count=b;
+        b=c;
+        c=count;
+    }
+    scanf("%s" ,a1);
+    for(i=0;i<3;i++)
+    {
+        if(a1[i]=='A')
+        {
+            b1[i]=a;
+        }
+        else if(a1[i]=='B')
+        {
+            b1[i]=b;
+        }
+        else if(a1[i]=='C')
+        {
+            b1[i]=c;
+        }
+    }
+    printf( "%d %d %d", b1[0], b1[1], b1[2] );
+    return 0;
+}
